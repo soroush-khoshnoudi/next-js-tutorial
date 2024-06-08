67043991210725898302e0ec13fd37213de10696
soroush-khoshnoudi <soroushkhoshnoudi@gmail.com>
2024-06-08 18:39:11 +0330

    complete the code

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -16,7 +16,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'edit',
+  title: 'login',
 };
 
 export default function LoginPage() {
